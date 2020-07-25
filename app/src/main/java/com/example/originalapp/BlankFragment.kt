5ff0d0ff54a769344f9706215d634161bf2eed09
c0117290 <c0117290@users.noreply.github.com>
2020-07-25 23:18:27 +0900

    7/27
    事件です次へと戻るが消えました

--- app/src/main/java/com/example/originalapp/BlankFragment.kt
+++ app/src/main/java/com/example/originalapp/BlankFragment.kt
@@ -5,17 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_blank.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+// FIXME: ダメだよここに宣言しては
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BlankFragment : Fragment() {
     // ここから
     // スライドショーで表示する画像の配列の定数
@@ -25,13 +20,15 @@ class BlankFragment : Fragment() {
     private var index = 0
     //ここまで
 
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    // TODO: ←はやるべきことを示します。
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
+        // FIXME: コピペでしょ！？IDがきちんとついてないよ。レイアウト側に
         //ここから
         imageView.setOnClickListener(this)
         nextButton.setOnClickListener(this)
@@ -53,16 +50,8 @@ class BlankFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_blank, container, false)
     }
 
+    // TODO: コピペ絶対ダメ。わからなくなる原因
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             BlankFragment().apply {
