0e8a0f4da91f6121abe8a697514da135229c7fa9
m.sato <Amagami914>
2020-08-26 22:21:15 +0900

    Nextボタン押下処理追加

--- app/src/main/java/com/example/originalapp/EnglishContentsActivity.kt
+++ app/src/main/java/com/example/originalapp/EnglishContentsActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.english_contents_activity.*
 
 class EnglishContentsActivity:AppCompatActivity() {
+    var page = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.english_contents_activity)
@@ -27,13 +28,8 @@ class EnglishContentsActivity:AppCompatActivity() {
         thnt_main_text.text ="May I recline seat?"
 
         next_button.setOnClickListener{
-            main_text.text = "魚料理/肉料理をお願いします"
-            en_main_text.text="I'd like fish/meat,please"
-            t_main_text.text ="オレンジジュースをください"
-            tnt_main_text.text ="I'd like orange juice, please"
-            th_main_text.text ="コーヒーをお願いします。砂糖とミルクもください。"
-            thnt_main_text.text ="Coffee, please. With some sugar and milk."
-
+            page += 1
+            pageGenerate(page)
         }
 
         /** 条件分岐 */
@@ -48,6 +44,24 @@ class EnglishContentsActivity:AppCompatActivity() {
                 titleImageView.setImageResource(R.drawable.aviation)
                 Log.d("例外", "title_numの値が不正な値です")
             }
+        }
+    }
+
+    private fun pageGenerate(page: Int) {
+        if (page == 0) {
+            main_text.text = "魚料理/肉料理をお願いします"
+            en_main_text.text="I'd like fish/meat,please"
+            t_main_text.text ="オレンジジュースをください"
+            tnt_main_text.text ="I'd like orange juice, please"
+            th_main_text.text ="コーヒーをお願いします。砂糖とミルクもください。"
+            thnt_main_text.text ="Coffee, please. With some sugar and milk."
+        } else if (page == 1) {
+            main_text.text = "魚料理/肉料理をお願いします2"
+            en_main_text.text="I'd like fish/meat,please2"
+            t_main_text.text ="オレンジジュースをください2"
+            tnt_main_text.text ="I'd like orange juice, please2"
+            th_main_text.text ="コーヒーをお願いします。砂糖とミルクもください。2"
+            thnt_main_text.text ="Coffee, please. With some sugar and milk.2"
         }
     }
 }
